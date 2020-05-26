0899ef5c5aec2b8855ad95206c401368e4ff78b6
Nick Howell <nick@nickhowell.co.uk>
2020-05-26 22:25:41 +0100

    need to include puppetlitmus to get `idempotent_apply`

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,5 +2,6 @@
 
 require 'puppet_litmus'
 require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_local.rb'))
+include PuppetLitmus
 
 PuppetLitmus.configure!
